52294ca71b730021490d7c37f0dd8c09eb8d66ef
Gabriel Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-06-30 19:11:11 -0400

    Fixed script so wasn't calling pacman on every package.

--- install/pkginstall.sh
+++ install/pkginstall.sh
@@ -10,12 +10,82 @@
 # 
 # ------------------------------------------------------------------------------
 
-pacman -S acpi
-pacman -S alsa-firmware
-pacman -S alsa-oss
-pacman -S alsa-plugins
-pacman -S alsa-tools
-pacman -S alsa-utils
+pacman -S acpi \
+       alsa-firmware \
+       alsa-oss \
+       alsa-plugins \
+       alsa-tools \
+       alsa-utils \
+       dmenu \
+       doxygen \
+       evince \
+       feh \
+       firefox \
+       gdb \
+       gimp \
+       git \
+       gohufont \
+       gparted \
+       gsimplecal \
+       i3-wm \
+       i3status \
+       ifplugd \
+       iproute2 \
+       iputils \
+       iw \
+       libpam-google-authenticator \
+       logrotate \
+       lsof \
+       moc \
+       nmap \
+       openssh \
+       openvpn \
+       os-prober \
+       otf-font-awesome \
+       pulseaudio \
+       pulseaudio-alsa \
+       python-gobject \
+       python-matplotlib \
+       qrencode \
+       redshift \
+       rsync \
+       rxvt-unicode \
+       scrot \
+       texinfo \
+       texlive-bibtexextra \
+       texlive-bin \
+       texlive-core \
+       texlive-fontsextra \
+       texlive-formatsextra \
+       texlive-latexextra \
+       texlive-publishers \
+       transmission-gtk \
+       ttf-dejavu \
+       ttf-droid \
+       ttf-inconsolata \
+       ttf-ms-fonts \
+       udisks \
+       unzip \
+       urxvt-perls \
+       vim \
+       vlc \
+       wget \
+       wpa_supplicant \
+       xbindkeys \
+       xcompmgr \
+       xorg-server \
+       xorg-server-utils \
+       xorg-xdm \
+       xorg-xev \
+       xorg-xhost \
+       xorg-xinit \
+       xorg-xkill \
+       xorg-xlsfonts \
+       xorg-xprop \
+       xorg-xwininfo \
+       youtube-dl-mp3 \
+       zsh
+
 # pacman -S android-platform
 # pacman -S android-sdk
 # pacman -S android-sdk-build-tools
@@ -29,101 +99,32 @@ pacman -S alsa-utils
 # pacman -S brother-mfc-j480dw
 # pacman -S cups
 # pacman -S device-mapper
-pacman -S dmenu
-pacman -S doxygen
-pacman -S evince
-pacman -S feh
-pacman -S firefox
-pacman -S gdb
-pacman -S gimp
-pacman -S git
-pacman -S gohufont
-pacman -S gparted
-pacman -S gsimplecal
-pacman -S i3-wm
-pacman -S i3status
-pacman -S ifplugd
 # pacman -S incron
 # pacman -S inetutils
 # pacman -S inotify-tools
 # pacman -S intltool
-pacman -S iproute2
-pacman -S iputils
-pacman -S iw
 # pacman -S jdk7-openjdk
-pacman -S libpam-google-authenticator
-pacman -S logrotate
-pacman -S lsof
-pacman -S moc
 # pacman -S net-tools
-pacman -S nmap
 # pacman -S openjdk7-src
-pacman -S openssh
-pacman -S openvpn
-pacman -S os-prober
-pacman -S otf-font-awesome
 # pacman -S pciutils
 # pacman -S pcmciautils
-pacman -S pulseaudio
-pacman -S pulseaudio-alsa
-pacman -S python-gobject
-pacman -S python-matplotlib
 # pacman -S python-pyserial
 # pacman -S python2-matplotlib
 # pacman -S python2-pyserial
 # pacman -S python2-virtualenv
 # pacman -S qiv
-pacman -S qrencode
-pacman -S redshift
-pacman -S rsync
-pacman -S rxvt-unicode
-pacman -S scrot
 # pacman -S strace
 # pacman -S sudo
 # pacman -S sysfsutils
 # pacman -S sysstat
 # pacman -S systemd-sysvcompat
-pacman -S texinfo
-pacman -S texlive-bibtexextra
-pacman -S texlive-bin
-pacman -S texlive-core
-pacman -S texlive-fontsextra
-pacman -S texlive-formatsextra
-pacman -S texlive-latexextra
-pacman -S texlive-publishers
 # pacman -S thunar
-pacman -S transmission-gtk
-pacman -S ttf-dejavu
-pacman -S ttf-droid
-pacman -S ttf-inconsolata
-pacman -S ttf-ms-fonts
-pacman -S udisks
-pacman -S unzip
-pacman -S urxvt-perls
 # pacman -S usbutils
 # pacman -S util-linux
 # pacman -S valgrind
 # pacman -S vi
-pacman -S vim
-pacman -S vlc
 # pacman -S vpnc
 # pacman -S wavpack
-pacman -S wget
-pacman -S wpa_supplicant
-pacman -S xbindkeys
-pacman -S xcompmgr
 # pacman -S xdotool
 # pacman -S xf86-input-synaptics
 # pacman -S xfsprogs
-pacman -S xorg-server
-pacman -S xorg-server-utils
-pacman -S xorg-xdm
-pacman -S xorg-xev
-pacman -S xorg-xhost
-pacman -S xorg-xinit
-pacman -S xorg-xkill
-pacman -S xorg-xlsfonts
-pacman -S xorg-xprop
-pacman -S xorg-xwininfo
-pacman -S youtube-dl-mp3
-pacman -S zsh
